8314f37ad0d646897ef63d8e8ef22bf453b74568
raaghu2002 <raaghu2002@gmail.com>
2025-03-18 10:44:41 +0530

    init

--- src/main/java/wizzybox/IDCard_Backend/Config/CloudinaryConfig.java
+++ src/main/java/wizzybox/IDCard_Backend/Config/CloudinaryConfig.java
@@ -5,14 +5,24 @@ import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class CloudinaryConfig {
+
+
     @Bean
     public Cloudinary cloudinary() {
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", "employeephoto");
+        config.put("api_key", "951773785157282");
+        config.put("api_secret", "MyPj0irxpBoFpH-omw34iiDXtA8");
+
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "employeephoto",
-                "api_key", "951773785157282",
-                "api_secret", "MyPj0irxpBoFpH-omw34iiDXtA8"
+                "cloud_name", config.get("cloud_name"),
+                "api_key", config.get("api_key"),
+                "api_secret", config.get("api_secret")
         ));
     }
 }
