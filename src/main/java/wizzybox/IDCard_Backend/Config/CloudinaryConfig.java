b10268e2fe49d46cf6a0401588de5e28d7576beb
raaghu2002 <raaghu2002@gmail.com>
2025-03-18 11:23:17 +0530

    init

--- src/main/java/wizzybox/IDCard_Backend/Config/CloudinaryConfig.java
+++ src/main/java/wizzybox/IDCard_Backend/Config/CloudinaryConfig.java
@@ -1,6 +1,7 @@
 package wizzybox.IDCard_Backend.Config;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,21 +11,32 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud-name}")
-    private String cloudName;
-
-    @Value("${cloudinary.api-key}")
-    private String apiKey;
-
-    @Value("${cloudinary.api-secret}")
-    private String apiSecret;
+//    @Value("${cloudinary.cloud-name}")
+//    private String cloudName;
+//
+//    @Value("${cloudinary.api-key}")
+//    private String apiKey;
+//
+//    @Value("${cloudinary.api-secret}")
+//    private String apiSecret;
+//
+//    @Bean
+//    public Cloudinary cloudinary() {
+//        return new Cloudinary(Map.of(
+//                "cloud_name", cloudName,
+//                "api_key", apiKey,
+//                "api_secret", apiSecret
+//        ));
+//    }
 
     @Bean
     public Cloudinary cloudinary() {
-        return new Cloudinary(Map.of(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret
+        return new Cloudinary(ObjectUtils.asMap(
+                "cloud_name", "employeephoto",
+                "api_key", "951773785157282",
+                "api_secret", "MyPj0irxpBoFpH-omw34iiDXtA8"
         ));
     }
+
+
 }
